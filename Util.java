657ef62332f10d04b3cf8547abcba07adf045cbe
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-11-18 23:42:42 +0530

    Create Util.java

---
+++ Util.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class DateExample {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Current Date and Time: " + date);
+    }
+}
